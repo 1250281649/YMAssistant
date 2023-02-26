7fd0d70509bdd79e36cceb788554bd7d9443e6a0
亦梦云烟 <1250281649@qq.com>
2023-02-26 15:12:26 +0800

    NEW: add more tool button and add ternimal button

--- python/YMAssistant.py
+++ python/YMAssistant.py
@@ -12,6 +12,7 @@ sys.path.append(os.getcwd() + os.path.sep + "thirdparty")
 from netron.server import start
 from ui.FlatWidget import FlatWidget
 import ui.Ui_MainFrame
+from functools import partial
 
 def timestr(d):
     if len(str(d))==1:
@@ -31,9 +32,13 @@ class MainFrame(QObject):
         self.MainWindow_.IconPushButton.setIcon(QIcon("assets/pictures/icon.png"))
         self.MainWindow_.MaxButton.setIcon(QIcon("assets/pictures/maximize1.png"))
         self.MainWindow_.SettingButton.setIcon(QIcon("assets/pictures/setting.png"))
+        self.MainWindow_.NetronToolButton.setIcon(QIcon("assets/pictures/netron.png"))
+        self.MainWindow_.TernimalToolButton.setIcon(QIcon("assets/pictures/ternimal.png"))
         self.MainWindow_.MaxButton.clicked.connect(self.Maximized)
         self.MainWindow_.MinButton.clicked.connect(self.Minimized)
         self.MainWindow_.TitleLabel.mouseDoubleClickEvent = self.TitleDClicked
+
+        self.ToolMenuButtonSetupEvent()
 
         # time
         update_time_thread = threading.Thread(target=self.UpdateTime)
@@ -73,6 +78,28 @@ class MainFrame(QObject):
 
     def Close(self):
         self.FlatWidget_.Close(None)
+
+    def ToolMenuButtonSetupEvent(self):
+        self.MenuToolButtons = [
+            self.MainWindow_.NetronToolButton,
+            self.MainWindow_.TernimalToolButton,
+        ]
+        for i in range(len(self.MenuToolButtons)):
+            self.MenuToolButtons[i].clicked.connect(partial(self.MenuToolButtonEvent, i))
+        self.MenuToolButtonEvent(0)
+
+    def MenuToolButtonEvent(self, index):
+        self.MainWindow_.DisplayStackedWidget.setCurrentIndex(index)
+        menu_style_str = '''
+            QToolButton::hover { color: rgb(220, 220, 220); background-color: rgb(38, 38, 38);}
+            QToolButton { color: rgb(160, 160, 160); border: 0px; background-color: rgb(20, 20, 20);}'''
+        menu_clicked_style_str = '''
+            QToolButton { color: rgb(234, 85, 4);border: 0px; background-color: rgb(0, 0, 0);}'''
+        for i in range(len(self.MenuToolButtons)):
+            if i==index:
+                self.MenuToolButtons[i].setStyleSheet(menu_clicked_style_str)
+            else:
+                self.MenuToolButtons[i].setStyleSheet(menu_style_str)
 
     # ----------------------------------------- UI end ----------------------------------------
     def SetCurrentTime(self):
